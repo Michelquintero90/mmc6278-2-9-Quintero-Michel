106923a6c91cfd7984a462aebdf8a4a01fdeda9f
michelq <michel.quintero24@gmail.com>
2025-03-23 22:03:12 -0400

    Fix to use dynamic port on Heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
